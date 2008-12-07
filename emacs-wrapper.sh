39abeddae620659b3689a397afd2dd984059f593
Ulrich Müller <ulm@gentoo.org>
2008-12-07 01:38:52 +0000

    Bug fix.
    svn path=/emacs-daemon/; revision=1171

--- emacs-wrapper.sh
+++ emacs-wrapper.sh
@@ -2,7 +2,7 @@
 
 # Start Emacs with a login shell wrapper to read the user's profile.
 export SHELL=${SHELL:-/bin/bash}
-exec -l "${SHELL}" -c "exec \"${EMACS}\" $@" </dev/null &>/dev/null &
+exec -l "${SHELL}" -c "exec \"${EMACS}\" $*" </dev/null &>/dev/null &
 pid=$!
 
 # Wait for Emacs daemon to detach
@@ -13,7 +13,7 @@ while [ ${timeout} -gt 0 ]; do
     timeout=$((${timeout} - 1))
 done
 
-echo "${0##*/}: timeout waiting for \"${EMACS} $@\" to detach" 1>&2
+echo "${0##*/}: timeout waiting for \"${EMACS} $*\" to detach" 1>&2
 pkill -P ${pid}
 kill ${pid} 2>/dev/null
 exit 1

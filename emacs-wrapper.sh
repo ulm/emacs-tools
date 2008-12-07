1c975ccd3ef7b6f429d7ce7301bc7f70acbef2af
Ulrich Müller <ulm@gentoo.org>
2008-12-07 14:42:43 +0000

    Add property.
    svn path=/emacs-daemon/; revision=1174

--- emacs-wrapper.sh
+++ emacs-wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Copyright 2008 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Id: $
+# $Id$
 
 # Start Emacs with a login shell wrapper to read the user's profile
 export SHELL=${SHELL:-/bin/bash}

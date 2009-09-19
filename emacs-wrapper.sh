6cc684e2ac29a131fd2fdcda42cbf49ffd80b284
Ulrich Müller <ulm@gentoo.org>
2009-09-19 07:46:58 +0000

    Save output in a temporary file and display in case of error.
    svn path=/emacs-daemon/; revision=1417

--- emacs-wrapper.sh
+++ emacs-wrapper.sh
@@ -3,8 +3,12 @@
 # Distributed under the terms of the GNU General Public License v2
 # $Id$
 
+# Save output in a temporary file and display in case of error
+logfile=$(mktemp ${TMPDIR:-/tmp}/emacs.log.XXXXXX)
+trap "rm -f '${logfile}'" EXIT
+
 # Start Emacs with a login shell wrapper to read the user's profile
-exec -l "${SHELL}" -c "exec \"${EMACS}\" $*" </dev/null &>/dev/null &
+exec -l "${SHELL}" -c "exec \"${EMACS}\" $*" </dev/null &>"${logfile}" &
 pid=$!
 
 # Wait for Emacs daemon to detach
@@ -12,10 +16,13 @@ for (( t=${EMACS_TIMEOUT:-30}; t > 0; t-- )); do
     sleep 1
     if ! kill -0 ${pid} 2>/dev/null; then
         wait ${pid}		# get exit status
-        exit
+        status=$?
+        [[ ${status} -ne 0 ]] && cat "${logfile}"
+        exit ${status}
     fi
 done
 
+cat "${logfile}"
 echo "${0##*/}: timeout waiting for ${EMACS} to detach" >&2
 kill ${pid} $(pgrep -P ${pid}) 2>/dev/null
 exit 1

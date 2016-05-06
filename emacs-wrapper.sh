7342873eb24c5d7c4ca4e4bda4230b0df402dff0
Ulrich Müller <ulm@gentoo.org>
2016-05-06 15:05:56 +0200

    Update copyright years.

--- emacs-wrapper.sh
+++ emacs-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2008-2014 Gentoo Foundation
+# Copyright 2008-2016 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2 or later
 
 # Save output in a temporary file and display in case of error
